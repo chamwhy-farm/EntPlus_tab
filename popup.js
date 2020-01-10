4ae3f45f254bc851011e0fed7c4664ba6885b33b
sjufkaus <sjufkaus@gmail.com>
2020-01-10 11:41:11 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-var bestUsers = ['chamwhy', 'dark', 'thoratica', 'lyh2315', 'y711825'];
+var bestUsers = ['chamwhy', 'dark', 'thoratica', 'lyh2315', 'y711825', 'ddtt786'];
 function form(username, type, btn){
   return getUserByUsername(username).then(function(data){
     var imgSrc = getUserImgLinkByUser(data);
